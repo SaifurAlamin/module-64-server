1dd944310d2e90e08ad437f7f015d395a78f24b8
SaifurAlamin <saifurrahman1614@gmail.com>
2022-08-29 14:29:31 +0600

    server side is ready

---
+++ _step.js
@@ -0,0 +1,15 @@
+/**
+ * create a folder (mkdir or manually)
+ * open command line to the folder
+ * npm init
+ * npm install express
+ * create index.js
+ * -----------------------
+ * * For express and node server
+ * --------------------
+ * 1.require express
+ * 2.create app
+ * 3. declare port
+ * 4. set get app.get
+ * 5. listen port
+ */
